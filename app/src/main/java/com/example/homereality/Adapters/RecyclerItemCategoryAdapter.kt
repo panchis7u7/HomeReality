374e7b39513310a2964b650ce0f96f21267f74f1
panchis7u7 <smadrigal.rod@gmail.com>
2021-04-15 11:18:58 -0500

    Implemented recycler view.

--- app/src/main/java/com/example/homereality/Adapters/RecyclerItemCategoryAdapter.kt
+++ app/src/main/java/com/example/homereality/Adapters/RecyclerItemCategoryAdapter.kt
@@ -1,16 +1,24 @@
 package com.example.homereality.Adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.homereality.Models.FurnitureCategory
+import com.example.homereality.R
 
 class RecyclerItemCategoryAdapter (private var context: Context,
                                    private var items: MutableList<FurnitureCategory>) :
 RecyclerView.Adapter<RecyclerItemCategoryAdapter.ItemHolder>(){
 
     inner class ItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        val imageViewIconCategory: ImageView = itemView.findViewById(R.id.imageViewIconCategory)
+        val textViewCategoria: TextView = itemView.findViewById(R.id.textViewCategory)
 
         init {
 
@@ -19,11 +27,20 @@ RecyclerView.Adapter<RecyclerItemCategoryAdapter.ItemHolder>(){
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
-        TODO("Not yet implemented")
+        return ItemHolder(LayoutInflater.from(parent.context).inflate(
+                R.layout.item_furniture_category_layout,
+                parent,
+                false
+        ))
     }
 
     override fun onBindViewHolder(holder: ItemHolder, position: Int) {
-        TODO("Not yet implemented")
+        var item: FurnitureCategory = items.get(position)
+        holder.textViewCategoria.text = item.category
+        Glide.with(holder.itemView)
+                .load(item.icon)
+                .into(holder.imageViewIconCategory)
+
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/homereality/MainActivity.kt
+++ app/src/main/java/com/example/homereality/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.homereality
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.homereality.Adapters.RecyclerItemCategoryAdapter
+import com.example.homereality.Models.FurnitureCategory
 import com.example.homereality.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +15,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         //setContentView(R.layout.activity_main)
         setContentView(binding.root)
+
+        binding.recyclerCategory.layoutManager = LinearLayoutManager(this,
+                RecyclerView.VERTICAL, false)
+        binding.recyclerCategory.adapter = RecyclerItemCategoryAdapter(this, populateList())
+
     }
+
+    private fun populateList(): MutableList<FurnitureCategory>{
+        var items: MutableList<FurnitureCategory> = mutableListOf()
+        items.add(FurnitureCategory("https://img.icons8.com/ios/452/test-passed.png",
+        "Prueba1"))
+        items.add(FurnitureCategory("https://img.icons8.com/ios/452/test-passed.png",
+                "Prueba2"))
+        items.add(FurnitureCategory("https://img.icons8.com/ios/452/test-passed.png",
+                "Prueba3"))
+        return items
+    }
+
 }
