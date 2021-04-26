ce37c5c35c76a1aacbfdcd0f422d4919be019f75
panchis7u7 <smadrigal.rod@gmail.com>
2021-04-26 13:31:48 -0500

    Styling python code.

--- FirebaseLoader/main.py
+++ FirebaseLoader/main.py
@@ -1,7 +1,7 @@
 '''
-Uploaded  furniture dataset to firebase
-
+Upload furniture dataset to firebase.
 '''
+###################################################################################
 
 import json
 from typing import List
@@ -10,11 +10,19 @@ from firebase_admin import credentials, storage, firestore
 from os import listdir
 from os.path import isfile, join
 
-#
+###################################################################################
+
+###################################################################################
+#Firebase API needs.
+
 STORAGE_PATH = "gs://homereality-7d0b8.appspot.com/"
 CERTIFICATE = "/home/panchis/Desktop/HomeReality/FirebaseLoader/key.json"
 DATA_PATH = "data"
 
+###################################################################################
+
+###################################################################################
+#Utility functions.
 
 def load_json(file_path):
     with open(file_path, 'r') as f:
@@ -29,17 +37,21 @@ def files_in_dir(mypath):
 def compare_records(doc1, doc2):
     return doc1.items() == doc2.items()
 
+###################################################################################
+
+###################################################################################
 
 def main():
-    # firebase
+
+    #Connect to firebase.
     cred = credentials.Certificate(CERTIFICATE)
     app = firebase_admin.initialize_app(cred, {'storageBucket': STORAGE_PATH})
     store = firestore.client()
-    doc_ref = store.collection(u'BEDS & MATTRESSES')
+    doc_ref = store.collection(u'Furniture')
     docs = doc_ref.get()
     bucket = storage.bucket("homereality-7d0b8.appspot.com")
 
-    #
+    #Load data for upload.
     path = DATA_PATH
     items_types = listdir(path)
 
@@ -55,7 +67,7 @@ def main():
             model = [item_file for item_file in item_files if item_file.endswith('.glb')][0]
             collection = [item_file for item_file in item_files if item_file.endswith('.json')][0]
 
-            # check whenevrer uploaded
+            #Check if uploaded.
             exists = False
 
             item_data = load_json(path + '/' + index + '/' + collection)
@@ -66,21 +78,23 @@ def main():
 
             if not exists:
                 print("Add:" + model)
-                # add record
+                #Add record.
                 doc_ref.add(item_data)
 
-                # add model
+                #Add model.
                 fileName = path + '/' + index + '/' + model
                 blob = bucket.blob('models/' + model)
                 blob.upload_from_filename(fileName)
             
-                # add images
+                #Add images.
                 for image in images:
                     fileName = path + '/' + index + '/' + image
                     blob = bucket.blob('images/' + image)
                     blob.upload_from_filename(fileName)
             else:
                 print("Skip:" + model)
+
+###################################################################################
 
 
 if __name__ == "__main__":
