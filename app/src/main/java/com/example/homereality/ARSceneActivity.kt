51b57f3ed3af9d137b8c0c2e2deb60792d39b578
panchis7u7 <smadrigal.rod@gmail.com>
2021-05-02 16:16:57 -0500

    Layout for AR Scene.

---
+++ app/src/main/java/com/example/homereality/ARSceneActivity.kt
@@ -0,0 +1,11 @@
+package com.example.homereality
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ARSceneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_a_r_scene)
+    }
+}
