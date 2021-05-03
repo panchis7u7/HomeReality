de5d0c7779e95670af1fe5df637ec57c26283d19
panchis7u7 <smadrigal.rod@gmail.com>
2021-05-03 13:33:49 -0500

    AR models rendered!

--- app/src/main/java/com/example/homereality/ARSceneActivity.kt
+++ app/src/main/java/com/example/homereality/ARSceneActivity.kt
@@ -1,30 +1,33 @@
 package com.example.homereality
 
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyCharacterMap
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.homereality.databinding.ActivityARSceneBinding
+import com.google.ar.core.Anchor
+import com.google.ar.core.ArCoreApk
+import com.google.ar.core.ArCoreApk.Availability
+import com.google.ar.core.ArCoreApk.InstallStatus
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.assets.RenderableSource
 import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.Renderable
 import com.google.ar.sceneform.ux.ArFragment
-import com.google.firebase.FirebaseApp
+import com.google.ar.sceneform.ux.TransformableNode
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import java.io.File
-import java.lang.Exception
+
 
 class ARSceneActivity : AppCompatActivity() {
     private var _binding: ActivityARSceneBinding? = null
     private val binding get() = _binding!!
     private var storage: FirebaseStorage? = null
-    private var renderable: ModelRenderable? = null
+    private val TAG: String = ARSceneActivity::class.java.simpleName
+    private val MIN_OPENGL_VERSION = 3.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,67 +35,60 @@ class ARSceneActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_a_r_scene)
         setContentView(binding.root)
 
-        var model: String? = ""
+        var arFragment: ArFragment = supportFragmentManager.findFragmentById(R.id.fragmentARScene) as ArFragment
+
+        var modelLocation: String? = ""
         intent?.let {
-            model = it.extras?.getString("model")
+            modelLocation = it.extras?.getString("model")
         }
 
-        /*FirebaseApp.initializeApp(this)
         storage = FirebaseStorage.getInstance()
-        storage?.let {
-            Toast.makeText(this, "Resource: ${model}", Toast.LENGTH_LONG)
-            var storageRef: StorageReference = it.getReference().child(model!!)
-            var file: File = File.createTempFile("model", "glb")
-            try {
-                storageRef!!.getFile(file).addOnSuccessListener {
-                    buildModel(file)
-                    var arFragment: ArFragment =
-                        supportFragmentManager.findFragmentById(R.id.fragmentARScene) as ArFragment
-                    arFragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
-                        var anchorNode: AnchorNode = AnchorNode(hitResult.createAnchor())
-                        anchorNode.renderable = renderable
-                        arFragment.arSceneView.scene.addChild(anchorNode)
-                    }
+        //Check if firebase storgage is up and ARCore is supported and up to date.
+        if (storage != null && modelLocation != "") {
+
+            var model: File = File.createTempFile("model", "glb")
+            var storageRef: StorageReference = storage!!.reference.child(modelLocation!!)
+            storageRef.getFile(model).addOnSuccessListener {
+
+                Log.d("Model Recieved", "-----------------------> Model Downloaded!")
+                Toast.makeText(arFragment.context, "Model Downloaded!", Toast.LENGTH_SHORT).show()
+                arFragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
+                    Log.d("AR init.", "-----------------------> Setting up AR!")
+                    var anchor: Anchor = hitResult.createAnchor()
+                    buildModel(model, arFragment, anchor)
                 }
-            } catch (e: Exception) {
-                e.stackTrace
+
             }
-        }*/
 
-        storage = FirebaseStorage.getInstance()
-        storage?.let {
-            var storageRef: StorageReference = it.reference.child(model!!)
-            var file: File = File.createTempFile("model", "glb")
-            storageRef.getFile(file).addOnSuccessListener {
-
-                var renderableSource: RenderableSource = RenderableSource.builder()
-                        .setSource(this, Uri.parse(file.path), RenderableSource.SourceType.GLB)
-                        .setRecenterMode(RenderableSource.RecenterMode.ROOT)
-                        .build()
-
-                ModelRenderable.builder()
-                        .setSource(this, renderableSource)
-                        .setRegistryId(file.path)
-                        .build()
-                        .thenAccept { modelRenderable ->
-                            Toast.makeText(this, "Model Built", Toast.LENGTH_LONG)
-                            renderable = modelRenderable
-                        }
-                Log.d("Model", "Model Built ---------------------------------------")
-                    var arFragment: ArFragment =
-                            supportFragmentManager.findFragmentById(R.id.fragmentARScene) as ArFragment
-                    arFragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
-                        var anchorNode: AnchorNode = AnchorNode(hitResult.createAnchor())
-                        anchorNode.renderable = renderable
-                        arFragment.arSceneView.scene.addChild(anchorNode)
-
-                }
-            }
+        } else {
+            finish()
         }
     }
 
-    private fun buildModel(file: File){
+    private fun buildModel(model: File, arFragment: ArFragment, anchor: Anchor) {
+        var renderableSource = RenderableSource
+                .builder()
+                .setSource(arFragment.context, Uri.parse(model.path), RenderableSource.SourceType.GLB)
+                .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+                .build()
 
+        ModelRenderable.builder()
+                .setSource(arFragment.context, renderableSource)
+                .setRegistryId(model.path)
+                .build()
+                .thenAccept { modelRenderable -> addNodeToScene(arFragment, anchor, modelRenderable) }
+                .exceptionally { throwable -> Toast.makeText(arFragment.context, "Error: ${throwable.message}",
+                        Toast.LENGTH_LONG).show();  null}
+    }
+
+    private fun addNodeToScene(arFragment: ArFragment, anchor: Anchor, renderable: Renderable) {
+        Log.d("NodeGen", "-----------------------> Adding node to scene!")
+        var anchorNode = AnchorNode(anchor)
+        var node = TransformableNode(arFragment.transformationSystem)
+        node.renderable = renderable
+        node.setParent(anchorNode)
+        arFragment.arSceneView.scene.addChild(anchorNode)
+        node.select()
     }
 
     override fun onDestroy() {
